51090106401f30e39c167a51d1df107de69ef712
Tucker Vassau <tuckv@hotmail.com>
2019-12-14 01:29:44 -0700

    Start Btn Populates quiz, finishing resets

---
+++ questions.js
@@ -0,0 +1,15 @@
+var questions = [{
+    question: "Which of these is the correct bracket for an array?",
+    choices: ["[]", "{}", "()", "<>"],
+    answer: 0
+  },
+  {
+    question: "Which of these indicate something is a string?",
+    choices: ["STRONG", "STRING", "STRANG", "THANG"],
+    answer: 1
+  },
+  {
+    question: "Not sure what to ask for this question, but choose c!",
+    choices: ["Not this one dumbass", "Maybe this one?", "HERES THE ANSWER", "Also not an answer"],
+    answer: 2
+  }];
--- script.js
+++ script.js
@@ -1,64 +1,96 @@
 //Javascript here
-var questions = [{
-    question: "Which of these is the correct bracket for an array?",
-    choices: ["[]", "{}", "()", "<>"],
-    answer: 0
-  },
-  {
-    question: "Which of these indicate something is a string?",
-    choices: ["STRONG", "STRING", "STRANG", "THANG"],
-    answer: 1
-  },
-  {
-    question: "Not sure what to ask for this question, but choose c!",
-    choices: ["Not this one dumbass", "Maybe this one?", "HERES THE ANSWER", "Also not an answer"],
-    answer: 2
-  }]
   var mainEl = document.getElementById("main");
   var questionEl = document.getElementById("question");
   var bodyEl = document.createElement("div");
-  var wordsPerMillisecond = 3000;
+  var wordsPerMillisecond = 4000;
   var i = 0;
-  
-  function speedRead() {
-    mainEl.append(bodyEl);
+  var finalScore = 0;
 
+  var score = (wordsPerMillisecond/1000)*(questions.length +1);
+
+  $(".startBtn").on("click", function(){
+    addButtons();
+    startTimer();
+    clearInterval(wordsPerMillisecond);
+  })
+  
+  function startTimer() {
+    console.log(score);
+    var timerTime = setInterval(function(){
+      console.log(score);
+      if (score > 0){
+        score--;
+        $("#score").text(score);
+      };
+    }, 1000);
+    changeQuestion(0);
+    speedRead();
+  };
+
+  function speedRead() {
       var poemInterval = setInterval(function() {
-        console.log(i + " before calling questions");
         if (i < questions.length){
-          questionEl.textContent = questions[i].question;
-          $("#answer0").text(questions[i].choices[0]);
-          $("#answer1").text(questions[i].choices[1]);
-          $("#answer2").text(questions[i].choices[2]);
-          $("#answer3").text(questions[i].choices[3]);
+          changeQuestion(i);
           if (i === 0) {
             $("#btn-choice-" + questions[i].answer).one("click", function(){
-              alert("YOU HAVE CHOSEN WISELY");
               $("#correctOrNot").text("CORRECT 1");
             })
+            $("#btn-choice-1").one("click", function(){$("#correctOrNot").text("INCORRECT"); $("#correctOrNot").addClass("Incorrect");})
+            $("#btn-choice-2").one("click", function(){$("#correctOrNot").text("INCORRECT"); $("#correctOrNot").addClass("Incorrect");})
+            $("#btn-choice-3").one("click", function(){$("#correctOrNot").text("INCORRECT"); $("#correctOrNot").addClass("Incorrect");})
           } else if (i === 1){
             $("#btn-choice-" + questions[i].answer).one("click", function(){
-              alert("YOU HAVE CHOSEN WISELY #2");
               $("#correctOrNot").text("CORRECT 2");
             })
+            $("#btn-choice-0").one("click", function(){$("#correctOrNot").text("INCORRECT");})
+            $("#btn-choice-2").one("click", function(){$("#correctOrNot").text("INCORRECT");})
+            $("#btn-choice-3").one("click", function(){$("#correctOrNot").text("INCORRECT");})
           } else if (i === 2){
-            console.log(questions[i].answer + "This is what the answer is supposed to be");
             $("#btn-choice-" + questions[i].answer).one("click", function(){
-              alert("YOU HAVE CHOSEN WISELY #3");
               $("#correctOrNot").text("CORRECT 3");
+              finalScore = score;
+              alert(finalScore);
+              resetGame();
+              return score
             })
+            $("#btn-choice-0").one("click", function(){$("#correctOrNot").text("INCORRECT");})
+            $("#btn-choice-1").one("click", function(){$("#correctOrNot").text("INCORRECT");})
+            $("#btn-choice-2").one("click", function(){$("#correctOrNot").text("INCORRECT");})
           }
-          // console.log(i + " before ++");
           i++;
-          // console.log(i + "after ++");
         } 
       }, wordsPerMillisecond);
   };
+  function changeQuestion (i) {
+    $("#question").text(questions[i].question);
+    $("#btn-choice-0").text(questions[i].choices[0]);
+    $("#btn-choice-1").text(questions[i].choices[1]);
+    $("#btn-choice-2").text(questions[i].choices[2]);
+    $("#btn-choice-3").text(questions[i].choices[3]);
+  };
 
+  function resetGame() {
+    for (var j = 0; j < 4; j++){
+      $("#btn-choice-" +j).empty();
+    };
+    $("#question").empty();
+    $("#correctOrNot").empty();
+    $("#main").empty();
+    score = (wordsPerMillisecond/1000)*(questions.length +1);
+    $("#score").empty();
+  }
 
-
-  $(".startBtn").on("click", function(){
-    // $(".populateQuiz").html("Hello there");
-    speedRead();
-    clearInterval(wordsPerMillisecond);
-  })
+  function addButtons() {
+    var q = $('<div class="row"><div id="question"></div></div>');
+    $("#main").append(q);
+    var a = $('<div class="row"><button type="button" id="btn-choice-0" class="btn btn-primary"></button></div>');
+    $("#main").append(a);
+    var b = $('<div class="row"><button type="button" id="btn-choice-1" class="btn btn-primary"></button></div>');
+    $("#main").append(b);
+    var c = $('<div class="row"><button type="button" id="btn-choice-2" class="btn btn-primary"></button></div>');
+    $("#main").append(c);
+    var d = $('<div class="row"><button type="button" id="btn-choice-3" class="btn btn-primary"></button></div>');
+    $("#main").append(d);
+    var e = $('<div class="row"><div id="correctOrNot"></div></div>');
+    $("#main").append(e);
+};
